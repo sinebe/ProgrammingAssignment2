e39cc4d61e1da0ccfbca7b2a2d83ed55794904c5
Enis Becirbegovic <enisbe@gmail.com>
2014-12-18 21:40:13 -0700

    first commit of cashing function file

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	  inv <- NULL
+	  set <- function(y) {
+		x <<- y
+		inv <<- NULL
+	  }
+	  get <- function() x
+	  setinv <- function(solv) inv <<- solv
+	  getinv <- function() inv
+	  list(set = set, get = get,
+		   setinv = setinv,
+		   getinv = getinv)
 }
 
 
@@ -12,4 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		  i <- x$getinv()
+		  if(!is.null(i)) {
+			message("getting cached data")
+			return(i)
+		  }
+		  data <- x$get()
+		  i <- solve(data, ...)
+		  x$setinv(i)
+		  i
 }
